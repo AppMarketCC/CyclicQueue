cce16edb845549d23ba602750c737f3a127d0888
jack chen <715306030@qq.com>
2021-07-09 11:09:15 +0800

    递归与非递归的转换

--- CyclicQueue/queueprocess.cpp
+++ CyclicQueue/queueprocess.cpp
@@ -143,4 +143,19 @@ int Ack(int m, int n)
 		return Ack(m - 1, Ack(m, m - 1));
 }//算法结束
 
+#define M 20
+#define N 20
+int Ackerman(int m, int n)
+{
+	int akm[M][N]; int i, j;
+	for (j = 0; j < N; j++) akm[0][j] = j + 1;
+	for (i = 1; i < m; i++)
+	{
+		akm[i][0] = akm[i - 1][1];
+		for (j = 1; j < N; j++)
+			akm[i][j] = akm[i - 1][akm[i][j - 1]];
+	}
+	return(akm[m][n]);
+}//算法结束
+
 
